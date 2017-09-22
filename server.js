6108899a5429b7a92ce7264b3e9f47645952c5be
uzma24 <inayauzma828@gmail.com>
2017-09-22 13:09:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
+app.get('article-one',unction(req,res) {
+    res.Send('article-one wil be requested and server here');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
