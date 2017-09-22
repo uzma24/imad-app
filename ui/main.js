55f058e90e686902b80b9f9203456a7b3471f48e
uzma24 <inayauzma828@gmail.com>
2017-09-22 15:55:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,13 @@
 console.log('Loaded!');
 
+var img = getElementById('madi');
+
+
+img.onclick = function() {
+    var interval = setinterval(moveRight,50);
+};
+marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft+5;
+    img.style.marginLeft = marginLeft + 'px';
+}
